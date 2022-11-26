9536350113b1e6fc662a2ca71c47c1ab33d6a7d9
aron-helu <aronmilahela@gmail.com>
2022-11-26 15:02:09 +0300

    Routing fix

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users
 
-  resources "splashes#index"
+  root "splashes#index"
   resources :categories do
     resources :payments
   end
