061a60fac0bbfc1c5c2b8eaa20aff57ed2745f22
aron-helu <aronmilahela@gmail.com>
2022-11-26 14:35:57 +0300

    Fix linters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
-  end
-
-  def authenticate_user!
-    if user_signed_in?
-      super
-    else
-      redirect_to splashes_path, notice: 'you need to sign in before continuing'
-    end
+    attributes = %i[name email password password_confirmation current_password]
+    devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
   end
 end
--- app/controllers/splashes_controller.rb
+++ app/controllers/splashes_controller.rb
@@ -1,12 +1,5 @@
 class SplashesController < ApplicationController
-  before_action :set_splash, only: %i[show edit update destroy]
-
-  # GET /splashes or /splashes.json
   def index
-    if current_user.nil?
-        render 'splashes/index'
-    else
-        redirect_to new_user_registration_path
-    end
+    redirect_to groups_path if user_signed_in?
   end
 end
